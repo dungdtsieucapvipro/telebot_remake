9c952ba72169fbba83867887f35db2a5cccbfa57
dungdtsieucapvipro <149314686+dungdtsieucapvipro@users.noreply.github.com>
2025-02-23 22:42:03 +0700

    thêm auto fetch

--- telegram/auto_fetch_stock.py
+++ telegram/auto_fetch_stock.py
@@ -1,10 +1,12 @@
+import logging
 from get_stock_sql import fetch_all_stock_data
 import time
 
+# Cấu hình logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-def countdown(t):
-    """Hàm đếm ngược thời gian từ 60 giây xuống 0."""
+def count_down(t):
+    # Hàm đếm ngược thời gian từ 60 giây xuống 0.
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}'.format(secs)
@@ -14,16 +16,20 @@ def countdown(t):
 
     print("\rĐếm ngược: 00:00")
 
-def main():
+def auto_fetch_stock():
+    logging.info("Bắt đầu hệ thống lấy dữ liệu tự động")
     
     while True:
         try:
+            logging.info("Chuẩn bị lấy dữ liệu...")
             
             fetch_all_stock_data()  # Gọi hàm để lấy dữ liệu
-            countdown(60)  # Đếm ngược 60 giây
+            count_down(60)  # Đếm ngược 60 giây
+            logging.info("Đã lấy và lưu dữ liệu thành công.")
             
         except Exception as e:
+            logging.error(f"Lỗi khi lấy dữ liệu: {str(e)}")
             time.sleep(60)  # Đợi 1 phút trước khi thử lại
 
 if __name__ == "__main__":
-    main() 
+    auto_fetch_stock() 
